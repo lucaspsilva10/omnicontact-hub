0dafadafea2db6f8ea4c017b476e84187595524b
Lucas Patricio da Silva <lucaspatriciodasilvalj@gmail.com>
2026-06-18 18:33:09 -0300

    Adiciona as rotas para os métodos de buscar cliente por id, atualizar cliente e deletar cliente no customers.route

--- backend-node/src/routes/customer.routes.ts
+++ backend-node/src/routes/customer.routes.ts
@@ -8,8 +8,20 @@ customerRoutes.get("/clientes", (req, res) => {
   return customerController.listar(req, res);
 });
 
+customerRoutes.get("/cliente/:id", (req,res)=>{
+  return customerController.buscarPorId(req,res);
+})
+
 customerRoutes.post("/clientes", (req, res) => {
   return customerController.criar(req, res);
+});
+
+customerRoutes.put("/clientes/:id", (req, res) => {
+  return customerController.atualizar(req, res);
+});
+
+customerRoutes.delete("/clientes/:id", (req, res) => {
+  return customerController.deletar(req, res);
 });
 
 export default customerRoutes;
