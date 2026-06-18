87778761a0d53c2138a80ea8d3486a09629a2063
Lucas Patricio da Silva <lucaspatriciodasilvalj@gmail.com>
2026-06-18 12:09:46 -0300

    Adiciona os métodos de buscar cliente por id, atualizar cliente e deletar cliente na classe de CostumerController

--- backend-node/src/controllers/CustomerController.ts
+++ backend-node/src/controllers/CustomerController.ts
@@ -8,9 +8,45 @@ export class CustomerController {
     return res.json(this.customerService.listarClientes());
   }
 
+  buscarPorId(req: Request, res: Response){
+    const id = String(req.params.id);
+
+    const cliente = this.customerService.buscarClientePorId(id);
+
+    if(!cliente){
+      return res.status(404).json({mensagem: "Cliente não encontrado"});
+    }
+
+    return res.json(cliente);
+  }
+
   criar(req: Request, res: Response) {
     const cliente = this.customerService.criarCliente(req.body);
 
     return res.status(201).json(cliente);
+  }
+
+  atualizar(req: Request, res: Response){
+    const id = String(req.params.id);
+
+    const cliente = this.customerService.autualizarCliente(id, req.body);
+
+    if(!cliente){
+      return res.status(404).json({mensagem: "Cliente não encontrado"});
+    }
+
+    return res.json(cliente);
+  }
+
+  deletar(req: Request, res: Response){
+    const id = String(req.params.id);
+
+    const cliente = this.customerService.deletarCliente(id);
+
+    if(!cliente){
+      return res.status(404).json({mensagem: "Cliente não encontrado"});
+    }
+
+    return res.status(204).send();
   }
 }
